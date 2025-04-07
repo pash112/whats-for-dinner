9a5bb49384a5553970261afc5d255dafe266ca9c
Pavlo Markovsky <pash199060@gmail.com>
2025-04-07 21:29:32 +0300

    Додано базові файли проекту

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("search-form");
+const recipesList = document.getElementById("recipes-list");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  // Зчитуємо введені інгредієнти
+  const ingredients = document.getElementById("ingredients").value.split(",");
+  const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients.join(
+    ","
+  )}&number=5&apiKey=99f0a760ddb34cf6904d0517fb9cfd7a`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const recipes = await response.json();
+
+    // Додаємо рецепти до списку
+    recipesList.innerHTML = recipes
+      .map(
+        (recipe) =>
+          `<li><h3>${recipe.title}</h3><img src="${recipe.image}" alt="${recipe.title}" /></li>`
+      )
+      .join("");
+  } catch (error) {
+    recipesList.innerHTML =
+      "<li>Сталася помилка при завантаженні рецептів.</li>";
+  }
+});
